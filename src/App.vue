534b96d7e19c69793b910e3c6616457f195f436a
cqroot <cqroot@outlook.com>
2023-01-18 17:35:44 +0800

    feat: add sider for displaying router menu

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import RouterMenu from "@/layouts/RouterMenu.vue";
 </script>
 
 <template>
-  <n-layout>
-    <n-layout-header style="height: 64px; padding: 24px" bordered>
-      Yiheyuan Road
-    </n-layout-header>
+  <n-notification-provider>
+    <n-message-provider>
+      <n-layout>
+        <n-layout-header style="height: 64px; padding: 24px" bordered>
+          Yiheyuan Road
+        </n-layout-header>
 
-    <n-layout has-sider style="height: calc(100vh - 64px)">
-      <n-layout-sider
-        collapse-mode="width"
-        :collapsed-width="0"
-        :width="240"
-        show-trigger="bar"
-        content-style="padding: 24px;"
-        bordered
-      >
-        <p>
-          Handian Bridge Handian Bridge Handian Bridge Handian Bridge Handian
-          Bridge
-        </p>
-      </n-layout-sider>
-      <n-layout-content content-style="padding: 24px;">
-        Pingshan Road
-        <router-view />
-      </n-layout-content>
-    </n-layout>
-  </n-layout>
+        <n-layout has-sider style="height: calc(100vh - 64px)">
+          <n-layout-sider
+            collapse-mode="width"
+            :collapsed-width="0"
+            :width="240"
+            show-trigger="bar"
+            bordered
+          >
+            <router-menu />
+          </n-layout-sider>
+          <n-layout-content content-style="padding: 24px;">
+            Pingshan Road
+            <router-view />
+          </n-layout-content>
+        </n-layout>
+      </n-layout>
+    </n-message-provider>
+  </n-notification-provider>
 </template>
---
+++ src/layouts/RouterMenu.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { computed, h } from "vue";
+import type { Component } from "vue";
+import { RouterLink, useRoute } from "vue-router";
+import { NIcon } from "naive-ui";
+import type { MenuOption } from "naive-ui";
+import router from "@/router";
+
+function renderIcon(icon: Component) {
+  return () => h(NIcon, null, { default: () => h(icon) });
+}
+
+const route = useRoute();
+const routes = router.getRoutes();
+const menuOptions = [] as MenuOption[];
+
+for (const r of routes) {
+  if (r.path === "/") {
+    continue;
+  }
+
+  menuOptions.push({
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            name: r.path.substring(1),
+          },
+        },
+        { default: () => r.name }
+      ),
+    key: r.path,
+    icon: renderIcon(r.meta.icon as Component),
+  });
+}
+
+const currentPath = computed(() => route.path);
+</script>
+
+<template>
+  <n-menu :options="menuOptions" :value="currentPath" />
+</template>
